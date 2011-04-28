0e64c58598e23cae26b54bcf6006afc4740774d0
Hongli Lai (Phusion) <hongli@phusion.nl>
2011-04-28 19:56:24 +0200

    More refactoring

--- test/spec_helper.rb
+++ test/spec_helper.rb
@@ -1,37 +1,69 @@
+require 'rubygems'
 require 'fileutils'
+require 'socket'
+require 'timeout'
+require 'json'
+
+TEST_SERVER_PORT = 3765
 
 module SpecHelper
 	ROOT = File.expand_path(File.dirname(__FILE__) + "/..")
 	
+	class BackgroundNodeProcess
+		attr_accessor :script_file, :pid
+		attr_accessor :input_file, :output_file, :error_file
+		
+		def closed?
+			return !@pid
+		end
+		
+		def output
+			if closed? || @output
+				return @output
+			else
+				return File.read(@output_file)
+			end
+		end
+		
+		def error
+			if closed? || @error
+				return @error
+			else
+				return File.read(@error_file)
+			end
+		end
+		
+		def kill
+			raise "Already closed" if closed?
+			Process.kill('SIGTERM', @pid)
+		end
+		
+		def close(kill = true)
+			raise "Already closed" if closed?
+			self.kill if kill
+			Process.waitpid(@pid)
+			@pid    = nil
+			@output = File.read(@output_file)
+			@error  = File.read(@error_file)
+			return [$?.exitstatus, @output, @error]
+		ensure
+			discard
+		end
+		
+		def discard
+			File.unlink(@script_file) rescue nil if @script_file
+			File.unlink(@input_file) rescue nil if @input_file
+			File.unlink(@output_file) rescue nil if @output_file
+			File.unlink(@error_file) rescue nil if @error_file
+			@script_file = @input_file = @output_file = @error_file = nil
+		end
+	end
+	
 	def eval_js(js, input = nil)
-		script_file = "/tmp/javascript-#{Process.pid}-#{Thread.current.object_id}.js"
-		input_file  = "/tmp/input-#{Process.pid}-#{Thread.current.object_id}.txt"
-		output_file = "/tmp/output-#{Process.pid}-#{Thread.current.object_id}.txt"
-		error_file  = "/tmp/error-#{Process.pid}-#{Thread.current.object_id}.txt"
-		File.open(script_file.strip, "w") do |f|
-			f.write(js)
-		end
-		File.open(input_file, "w") do |f|
-			f.write(input) if input
-		end
-		File.open(output_file, "w").close
-		File.open(error_file, "w").close
-		pid = fork do
-			ENV['NODE_PATH'] = "#{ROOT}/lib"
-			STDIN.reopen(input_file, "r")
-			STDOUT.reopen(output_file, "w")
-			STDERR.reopen(error_file, "w")
-			exec("node", script_file)
-		end
-		Process.waitpid(pid)
-		return [$?.exitstatus,
-			File.read(output_file),
-			File.read(error_file)]
+		proc = async_eval_js(js, input)
+		return proc.close(false)
 	ensure
-		File.unlink(script_file) rescue nil if script_file
-		File.unlink(input_file) rescue nil if input_file
-		File.unlink(output_file) rescue nil if output_file
-		File.unlink(error_file) rescue nil if error_file
+		proc.discard if proc
 	end
 	
 	def eval_js!(js, input = nil)
@@ -43,6 +75,96 @@ module SpecHelper
 				"Output:\n  #{output}\n" +
 				"Error:\n  #{error}"
 		end
+	end
+	
+	def async_eval_js(js, input = nil)
+		@_counter = 0 if !@_counter
+		@_counter += 1
+		tail = "#{Process.pid}-#{Thread.current.object_id}-#{@_counter}"
+		proc = BackgroundNodeProcess.new
+		proc.script_file = "/tmp/javascript-#{tail}.js"
+		proc.input_file  = "/tmp/input-#{tail}.txt"
+		proc.output_file = "/tmp/output-#{tail}.txt"
+		proc.error_file  = "/tmp/error-#{tail}.txt"
+		File.open(proc.script_file, "w") do |f|
+			f.write(js.strip)
+		end
+		File.open(proc.input_file, "w") do |f|
+			f.write(input) if input
+		end
+		File.open(proc.output_file, "w").close
+		File.open(proc.error_file, "w").close
+		proc.pid = fork do
+			ENV['NODE_PATH'] = "#{ROOT}/lib"
+			STDIN.reopen(proc.input_file, "r")
+			STDOUT.reopen(proc.output_file, "w")
+			STDERR.reopen(proc.error_file, "w")
+			exec("node", proc.script_file)
+		end
+		return proc
+	rescue Exception => e
+		proc.discard if proc
+		raise e
+	end
+	
+	def wait_for_port(port = TEST_SERVER_PORT)
+		Timeout.timeout(5) do
+			while true
+				begin
+					puts 'connecting'
+					socket = TCPSocket.new('127.0.0.1', port)
+					puts 'connected'
+					socket.sync = true
+					return socket
+				rescue Errno::ECONNREFUSED
+					puts 'refused'
+					sleep 0.1
+				end
+			end
+		end
+	rescue Timeout::Error
+		raise "Cannot connect in time"
+	end
+	
+	def read_json(socket = @connection)
+		return JSON.parse(socket.readline)
+	end
+	
+	def write_json(socket, object = nil)
+		if !socket.respond_to?(:write)
+			object = socket
+			socket = @connection
+		end
+		socket.write("#{JSON.generate(object)}\n")
+	end
+	
+	def socket_readable?(socket)
+		return !!select([socket], nil, nil, 0)
+	end
+	
+	def should_never_happen(max_wait = 0.5, sleep_time = 0.001)
+		deadline = Time.now + max_wait
+		while Time.now < deadline
+			result = yield
+			if result
+				violated "Something that should never happen happened anyway"
+			else
+				sleep(sleep_time)
+			end
+		end
+	end
+	
+	def eventually(max_wait = 0.5, sleep_time = 0.001)
+		deadline = Time.now + max_wait
+		while Time.now < deadline
+			result = yield
+			if result
+				return result
+			else
+				sleep(sleep_time)
+			end
+		end
+		violated "Something that should eventually happen never happened"
 	end
 end
 
