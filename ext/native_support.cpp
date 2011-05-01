c596e9d8f28b490049125b7a9bc22528a8a74ee8
Hongli Lai (Phusion) <hongli@phusion.nl>
2011-05-01 17:07:40 +0200

    Add C++ wrapper for posix_fadvise()

--- ext/native_support.cpp
+++ ext/native_support.cpp
@@ -1,13 +1,21 @@
 #include <v8.h>
+#include <eio.h>
+#include <node.h>
+#include <cstdio>
+#include <cerrno>
+#include <fcntl.h>
+
+#include <config.h>
 
 using namespace v8;
+using namespace node;
 
 static Handle<Value>
 disableStdioBuffering(const Arguments &args) {
 	HandleScope scope;
 	setvbuf(stdout, NULL, _IONBF, 0);
 	setvbuf(stderr, NULL, _IONBF, 0);
-	return args.This();
+	return Undefined();
 }
 
 static Handle<Value>
@@ -15,8 +23,96 @@ flushStdio(const Arguments &args) {
 	HandleScope scope;
 	fflush(stdout);
 	fflush(stderr);
-	return args.This();
+	return Undefined();
 }
+
+
+struct PosixFadviseData {
+	int   fd;
+	int   advise;
+	off_t offset;
+	off_t len;
+	Persistent<Function> *callback;
+};
+
+static int
+posixFadviseWrapper(eio_req *req) {
+	#ifdef HAVE_POSIX_FADVISE
+		req->result = posix_fadvise(data.fd, data.offset, data.len, data.advise);
+	#else
+		req->result = -1;
+		errno = ENOSYS;
+	#endif
+		return 0;
+}
+
+static int
+posixFadviseDone(eio_req *req) {
+	HandleScope scope;
+	
+	PosixFadviseData *data = (PosixFadviseData *) req->data;
+	Persistent<Function> *callback = cb_unwrap(data->callback);
+	ev_unref(EV_DEFAULT_UC);
+	
+	Local<Value> arg;
+	if (req->result == -1) {
+		arg = ErrnoException(req->errorno);
+	} else {
+		arg = Local<Value>::New(Undefined());
+	}
+	
+	TryCatch try_catch;
+	(*callback)->Call(Context::GetCurrent()->Global(), 1, &arg);
+	if (try_catch.HasCaught()) {
+		FatalException(try_catch);
+	}
+	
+	cb_destroy(callback);
+	delete data;
+	
+	return 0;
+}
+
+// fd, offset, len, advise, callback
+static Handle<Value>
+posixFadvise(const Arguments &args) {
+	HandleScope scope;
+	PosixFadviseData *data = new PosixFadviseData();
+	eio_req *req;
+	
+	if (args.Length() < 4 || !args[0]->IsInt32() || !args[1]->IsInt32()
+	 || !args[2]->IsInt32() || !args[3]->IsInt32()) {
+		return ThrowException(Exception::TypeError(String::New("Bad argument")));
+	}
+	
+	data->fd     = args[0]->Int32Value();
+	data->offset = (off_t) args[1]->Int32Value();
+	data->len    = (off_t) args[2]->Int32Value();
+	data->advise = args[3]->Int32Value();
+	
+	if (args[4]->IsFunction()) {
+		data->callback = cb_persist(args[4]);
+		req = eio_custom(posixFadviseWrapper, EIO_PRI_DEFAULT, posixFadviseDone, data);
+		assert(req);
+		ev_ref(EV_DEFAULT_UC);
+		return Undefined();
+	} else {
+		#ifdef HAVE_POSIX_FADVISE
+			int ret = posix_fadvise(data->fd, data->offset, data->len, data->advise);
+			int e = errno;
+			delete data;
+			if (ret == -1) {
+				return ThrowException(ErrnoException(errno));
+			} else {
+				return Undefined();
+			}
+		#else
+			delete data;
+			return ThrowException(ErrnoException(ENOSYS));
+		#endif
+	}
+}
+
 
 extern "C" void
 init(Handle<Object> target) {
@@ -25,4 +121,21 @@ init(Handle<Object> target) {
 		FunctionTemplate::New(disableStdioBuffering)->GetFunction());
 	target->Set(String::NewSymbol("flushStdio"),
 		FunctionTemplate::New(flushStdio)->GetFunction());
+	
+	target->Set(String::NewSymbol("posix_fadvise"),
+		FunctionTemplate::New(posixFadvise)->GetFunction());
+	#ifdef HAVE_POSIX_FADVISE
+		target->Set(String::NewSymbol("POSIX_FADV_NORMAL"),
+			Integer::New(POSIX_FADV_NORMAL));
+		target->Set(String::NewSymbol("POSIX_FADV_SEQUENTIAL"),
+			Integer::New(POSIX_FADV_SEQUENTIAL));
+		target->Set(String::NewSymbol("POSIX_FADV_RANDOM"),
+			Integer::New(POSIX_FADV_RANDOM));
+		target->Set(String::NewSymbol("POSIX_FADV_WILLNEED"),
+			Integer::New(POSIX_FADV_WILLNEED));
+		target->Set(String::NewSymbol("POSIX_FADV_DONTNEED"),
+			Integer::New(POSIX_FADV_DONTNEED));
+		target->Set(String::NewSymbol("POSIX_FADV_NOREUSE"),
+			Integer::New(POSIX_FADV_NOREUSE));
+	#endif
 }
